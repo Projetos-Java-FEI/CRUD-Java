94b2a39f9392ca54bb633e287365b23032a96b17
Giovanni Antonio Moreira <giovannimoreira131@gmail.com>
2024-10-05 09:13:41 -0300

    Add files via upload

---
+++ criptoFei/src/criptofei/CriptoFei.java
@@ -0,0 +1,12 @@
+package criptofei;
+import view.JanelaInicial;
+
+
+public class CriptoFei {
+
+    public static void main(String[] args) {
+        JanelaInicial jInicial = new JanelaInicial();
+        jInicial.setVisible(true);
+    }
+    
+}
